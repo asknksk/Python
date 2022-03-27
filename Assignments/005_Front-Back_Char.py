dda807e76d6743e35299f1c9c7247d08828a9675
askin <asknkskk@gmail.com>
2022-03-27 15:19:27 +0300

    Front-Back_Char updated.

--- Assignments/005_Front-Back_Char.py
+++ Assignments/005_Front-Back_Char.py
@@ -4,8 +4,9 @@ Given a string, return a new string where the first and last chars have been exc
 Example :
 - print(front_back('clarusway'))  output => ylaruswac
 - print(front_back('ab'))   output => ba
+- print(front_back('a'))  output => a
 """
 def front_back(word) :
-    return word[-1] + word[1:-1] + word[0]
+    return word[-1] + word[1:-1] + word[0] if len(word) > 1 else word
 
 print(front_back('clarusway'))
